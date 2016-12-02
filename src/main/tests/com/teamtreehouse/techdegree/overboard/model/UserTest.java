54faf41425e2c3fdfc366f09c0e84a64cfb00f34
David Riesz <dbriesz@users.noreply.github.com>
2016-12-02 10:17:05 -0800

    Wrote test to assert that answerer's reputation goes up by 10 points if their answer is upvoted.

--- src/main/tests/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ src/main/tests/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -11,6 +11,10 @@ public class UserTest {
 
     private User user1;
     private User user2;
+    private Question question1;
+    private Answer answer1;
+    private Post post1;
+    private Post post2;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -21,15 +25,27 @@ public class UserTest {
         Board board = new Board(topic);
         user1 = new User(board, topic);
         user2 = new User(board, topic);
+
+        question1 = new Question(user1, "Differences between HashMap and Hashtable?");
+        post1 = user1.askQuestion(String.valueOf(question1));
+
+        answer1 = new Answer(question1, user2, "Hashtable is synchronized, whereas HashMap is not...");
+        post2 = user2.answerQuestion(question1, String.valueOf(answer1));
     }
 
     @Test
-    public void upvotingQuestionReturnsCorrectIncreaseInRep() throws Exception {
-        Question question = user1.askQuestion("Differences between HashMap and Hashtable?");
-        Post post = question;
-        user2.upVote(post);
-        int reputation = user1.getReputation();
+    public void questionUpvoteReturnsCorrectRepIncrease() throws Exception {
+        user2.upVote(post1);
+        int reputation1 = user1.getReputation();
 
-        assertEquals(5, reputation);
+        assertEquals(5, reputation1);
+    }
+
+    @Test
+    public void answerUpvoteReturnsCorrectRepIncrease() throws Exception {
+        user1.upVote(post2);
+        int reputation2 = user2.getReputation();
+
+        assertEquals(10, reputation2);
     }
 }
