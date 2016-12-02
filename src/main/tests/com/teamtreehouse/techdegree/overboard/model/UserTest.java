8eb32cec240ea19cd37da1668a5e78fd1b9c54fa
David Riesz <dbriesz@users.noreply.github.com>
2016-12-02 09:39:41 -0800

    Wrote test to ensure that questioner's reputation goes up by 5 points if their question is upvoted.

--- src/main/tests/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ src/main/tests/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -1,11 +1,35 @@
 package com.teamtreehouse.techdegree.overboard.model;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
 public class UserTest {
 
+    private User user1;
+    private User user2;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void setUp() throws Exception {
+        String topic = "Getting Started in Java";
+        Board board = new Board(topic);
+        user1 = new User(board, topic);
+        user2 = new User(board, topic);
+    }
+
     @Test
-    
+    public void upvotingQuestionReturnsCorrectIncreaseInRep() throws Exception {
+        Question question = user1.askQuestion("Differences between HashMap and Hashtable?");
+        Post post = question;
+        user2.upVote(post);
+        int reputation = user1.getReputation();
+
+        assertEquals(5, reputation);
+    }
 }
